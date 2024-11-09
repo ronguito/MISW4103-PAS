feaea8b719caf346953eb95adfdf1e8702f46b22
Raul Ramos <r.ramosg@uniandes.edu.co>
2024-11-09 16:54:52 -0500

    implementado escenario f01e01 editar titulo con Cypress

--- cypress/e2e/F01.cy.js
+++ cypress/e2e/F01.cy.js
@@ -1,13 +1,52 @@
+import Webpage from './src/webpage';
+import { beforeEach } from 'mocha';
+
+const { faker } = require('@faker-js/faker');
+
+const config = Cypress.env('config');
+
 describe('F01: Configurar opciones generales del sitio ', () => {
-	
+    
+    const wp = new Webpage();
+
+    beforeEach(() => {
+        //Give:
+        wp.visit(config.UrlLogin);
+        wp.login(config.UserName, config.UserPass);
+    });
     
     it('F01E01: Configurar el título del sitio', () => {
 		
-	
+        const title = faker.company.name();
+        //Give:
+        wp.openSiteSetting();
+        
+        //When
+        wp.clickEditSection("title-and-description");
+        wp.setPageTitle(title);
+        wp.clickOnButton("Save");
+        wp.closeSiteSetting();
+        
+        //then
+        wp.visit(config.UrlPublic);
+        wp.shouldContain(title);
+        
     });
-    
+    /*
     it('F01E02: Configurar la descripción del sitio', () => {
-		
+        const title = faker.company.name();
+        //Give:
+        wp.openSiteSetting();
+        
+        //When
+        wp.clickEditSection("title-and-description");
+        wp.setPageTitle(title);
+        wp.clickOnButton("Save");
+        wp.closeSiteSetting();
+        
+        //then
+        wp.visit(config.UrlPublic);
+        wp.shouldContain(title);		
 	
     });
     
@@ -20,5 +59,5 @@ describe('F01: Configurar opciones generales del sitio ', () => {
 		
 	
     });
-    
+    */
 });
--- cypress/e2e/src/webpage.js
+++ cypress/e2e/src/webpage.js
@@ -10,7 +10,80 @@ class Webpage {
         cy.get('button[type="submit"]').click();
         cy.url().should('include', '/ghost/#/dashboard');
     }
-	
+
+    openSiteSetting(){
+        cy.get('a[data-test-nav="settings"]').first().click();
+        cy.url().should('include', '/ghost/#/settings');     
+    }
+
+    closeSiteSetting(){
+        cy.get('button[data-testid="exit-settings"]').first().click();
+        cy.url().should('include', '/ghost/#/dashboard');     
+    }
+
+    clickEditSection(title){
+        cy.get(`div[data-testid="${title}"]`).find('button').click();        
+    }
+
+    setPageTitle(text){
+        cy.get('input[placeholder="Site title"]').clear().type(text);
+    }
+
+    setPageDescription(text){
+        cy.get('input[placeholder="Site description"]').clear().type(text);
+    }
+
+    clickOnButton(accion)
+    {
+        cy.get('button').then((buttons) => {
+            let matched = null;
+        
+            // Iterar sobre los botones para encontrar el que contiene "Save"
+            buttons.each((index, button) => {
+                if (button.innerText.includes(accion)) {
+                    matched = button;
+                    return false; // Rompe el ciclo each de Cypress
+                }
+            });
+        
+            // Lanzar un error si no se encuentra el botón
+            if (!matched) {
+                throw new Error(`No se encontró un botón para ${accion}`);
+            }
+        
+            // Hacer clic en el botón encontrado
+            cy.wrap(matched).click();
+            cy.wrap(matched).should('not.exist');
+            
+        });
+    }
+
+    clickOnPanel(name){
+        cy.get(`button[title="${name}"]`).click();
+    }
+
+    clickOnPickColor(){
+        cy.get(`button[aria-label="Pick color"]`).click();
+    }
+
+    setColor(color){
+        cy.get('input[aria-label="Color value"]').clear().type(text);
+    }
+
+    shouldContain(text){
+        cy.get('body').then((container) => {
+            const containerText = container.text();
+            expect(containerText).to.include(text);
+        });
+    }
+
+    shouldContainColor(color){
+        cy.document().then((doc) => {
+            const headContent = doc.documentElement.innerHTML;
+            const colorh = `#${color}`;
+            expect(headContent).to.include(color, `El head debe contener el color ${colorh}`);
+        });
+    }
 }
 
 export default Webpage;
