56ba3def641860cbb0ffeb126a37a4d558cddecb
Raul Ramos <r.ramosg@uniandes.edu.co>
2024-11-16 22:55:52 -0500

    Agragada validacion carpeta compare en vrt-kraken

--- vrt-kraken.js
+++ vrt-kraken.js
@@ -41,7 +41,9 @@ async function executeTest(){
             diffBounds: data.diffBounds,
             analysisTime: data.analysisTime
         }
-
+        if(!fs.existsSync('./results/kraken/compare')){
+            fs.mkdirSync('./results/kraken/compare', { recursive: true });
+        }
         fs.writeFileSync(`./results/kraken/compare/${ss68}.png`, data.getBuffer());
         
     }
